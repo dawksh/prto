ea2880b3bc69e34dd88afc0ba3a8f3309efb7a2e
daksh <akshd@outlook.com>
2025-06-06 16:15:22 +0530

    feat: add basic porto

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,34 @@
 "use client"
 
 import { Button } from "@/components/ui/button";
-import { useConnect } from "wagmi";
+
+import { useAccount, useConnect, useDisconnect } from "wagmi";
 
 export default function Home() {
   const { connect, connectors } = useConnect()
+  const { address, isConnected } = useAccount()
+  const { disconnect } = useDisconnect()
   return (
     <div className="flex flex-col gap-4 items-center justify-center h-screen">
-      <p className="text-2xl font-bold">Available Connectors:</p>
-      {connectors.map((connector) => (
-        <div key={connector.id} className="flex flex-col gap-2 items-center justify-center">
-          {connector.icon && <img src={connector.icon} className="w-10 h-10" alt="icon" />}
-          <Button onClick={() => connect({ connector })}>
-            {connector.name}
-          </Button>
+      {!isConnected && (
+        <div>
+          <p className="text-2xl font-bold">Available Connectors:</p>
+          {connectors.map((connector) => (
+            <div key={connector.id} className="flex flex-col gap-2 items-center justify-center">
+              {connector.icon && <img src={connector.icon} className="w-10 h-10" alt="icon" />}
+              <Button onClick={() => connect({ connector })}>
+                {connector.name}
+              </Button>
+            </div>
+          ))}
         </div>
-      ))}
+      )}
+      {isConnected && (
+        <div className="flex flex-col items-center gap-2">
+          <p>Connected to <span className="font-bold">{address}</span></p>
+          <Button onClick={() => disconnect()}>Disconnect</Button>
+        </div>
+      )}
     </div>
   );
 }
--- lib/config.ts
+++ lib/config.ts
@@ -1,8 +1,14 @@
-import { http, createConfig } from 'wagmi'
+import { http, createConfig, createStorage } from 'wagmi'
 import { baseSepolia } from 'wagmi/chains'
+import { porto } from 'porto/wagmi'
+import { localStorage } from 'porto/Storage'
 
 export const config = createConfig({
     chains: [baseSepolia],
+    connectors: [porto()],
+    storage: createStorage({
+        storage: localStorage(),
+    }),
     transports: {
         [baseSepolia.id]: http(),
     },
